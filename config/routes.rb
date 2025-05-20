930da8e2a071ba0d2cb8c646b858c04da9255bfe
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-05-20 12:02:35 +0900

    イベント作成ページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "static_pages#home"
   devise_for :users
   resources :users, only: [ :show ] do
-    resources :events, only: [ :new, :create, :edit, :update, :destroy ]
+    resources :events, only: [ :new, :create, :show, :edit, :update, :destroy ]
   end
   resources :events, only: [ :index ]
 end
---
+++ spec/system/events_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "イベント機能", type: :system do
+  let(:user) { FactoryBot.create(:user) }
+  let(:category) { FactoryBot.create(:category, name: "夏") }
+
+  before do
+    sign_in user
+    visit root_path
+  end
+
+  scenario "ユーザーはイベントを作成できる" do
+    expect {
+      click_link "イベント作成"
+      fill_in "タイトル", with: "花火大会"
+      fill_in "説明文", with: "みんなで夏を感じましょう！"
+      fill_in "開催日時", with: "2025-07-07"
+      fill_in "開催場所", with: "河川敷"
+      click_button "作成"
+
+      aggregate_failures do
+        expect(page).to have_content "イベントを作成しました。"
+        expect(page).to have_content "花火大会"
+        expect(page).to have_content "#{user.name}"
+      end
+    }.to change(user.events, :count).by(1)
+  end
+end
